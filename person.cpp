e64657f001d6bf61e03e253b3c3782667082f3a1
noraAngelova <73758640+noraAngelova@users.noreply.github.com>
2026-03-25 18:06:39 +0200

    Create person.cpp

---
+++ person.cpp
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "person.hpp"
+#include <iostream>
+#include <cstring>
+
+void Person::copy(char const* name) {
+	firstName = new char[strlen(name) + 1];
+	strcpy(firstName, name);
+}
+
+void Person::print() const {
+	std::cout << firstName << " " << age;
+}
+
+Person::Person(char const * name, unsigned const& ageData) {
+	age = ageData;
+	copy(name);
+}
+
+Person::~Person() {
+	delete[] firstName;
+}
+
+Person::Person(Person const& other) { 
+	age = other.age;
+	copy(other.firstName);
+}
+
+Person::Person(Person && other) {
+	std::cout << "Move constructor" << std::endl;
+	age = other.age;
+	firstName = other.firstName;
+	
+	other.firstName = nullptr;
+	other.age = 0;
+}
+
+Person& Person::operator=(Person const& other) {
+	if (this != &other) {
+		age = other.age;
+
+		delete[] firstName;
+		copy(other.firstName);
+	}
+
+	return *this;
+}
+
+Person& Person::operator=(Person && other) {
+	if (this != &other) {
+		age = other.age;
+
+		delete[] firstName;
+		firstName = other.firstName;
+
+		other.firstName = nullptr;
+		other.age = 0;
+	}
+
+	return *this;
+}
